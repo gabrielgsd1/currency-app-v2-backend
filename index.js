15283dcfa6293ea27557e8978d3245cfb32b42cb
unknown <gabrielgsd03@gmail.com>
2022-07-01 21:45:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,189 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const bcrypt = __importStar(require("bcrypt"));
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const axios = require('axios').default;
+const prisma = new client_1.PrismaClient();
+app.use(express.json());
+app.use(cors());
+async function usePrisma(callback) {
+    await prisma.$connect();
+    await callback();
+    await prisma.$disconnect();
+}
+async function hashPassword(password) {
+    const hashedPassword = await bcrypt.hash(password, 1);
+    return hashedPassword;
+}
+async function comparePassword(password, hashToCheck) {
+    const isPasswordValid = await bcrypt.compare(password, hashToCheck);
+    return isPasswordValid;
+}
+async function registerUser({ name, email, password }) {
+    const hashedPassword = await hashPassword(password);
+    const dataToSend = {
+        name,
+        email,
+        password: hashedPassword
+    };
+    let createdUser = null;
+    await usePrisma(async () => {
+        const dbUser = await prisma.user.create({
+            data: dataToSend
+        });
+        createdUser = {
+            id: dbUser.id,
+            name: dbUser.name,
+            email: dbUser.email
+        };
+    });
+    if (createdUser != null) {
+        return createdUser;
+    }
+    else {
+        return {
+            error: true,
+            message: 'Error on creating user!'
+        };
+    }
+}
+async function checkUser({ email, password }) {
+    let user = [];
+    await usePrisma(async () => {
+        user = await prisma.user.findMany({
+            where: {
+                email
+            }
+        });
+    });
+    const isLoginValid = await comparePassword(password, user[0].password);
+    if (isLoginValid) {
+        return {
+            id: user[0].id,
+            name: user[0].name,
+            email: user[0].email
+        };
+    }
+    else {
+        return {
+            error: true,
+            message: 'Wrong credentials!'
+        };
+    }
+}
+async function retrieveUser(id) {
+    let user = [];
+    console.log(user);
+    await usePrisma(async () => {
+        const dbSearch = await prisma.user.findMany({
+            where: {
+                id: id
+            }
+        });
+        user = dbSearch;
+    });
+    return user[0];
+}
+app.post('/registerUser', async (req, res) => {
+    const data = req.body;
+    try {
+        const userCreation = await registerUser(data);
+        res.json(userCreation);
+    }
+    catch (e) {
+        res.status(400).json(e);
+    }
+});
+app.post('/login', async (req, res) => {
+    const data = req.body;
+    const result = await checkUser(data);
+    res.json(result);
+});
+app.post('/convert', async (req, res) => {
+    const { from, to, id } = req.body;
+    if (typeof from == null || typeof to == null) {
+        return res.status(400).json({
+            error: true,
+            message: "Please select the coins to convert!"
+        });
+    }
+    try {
+        const response = await axios.get(`https://api.getgeoapi.com/v2/currency/convert?api_key=${process.env.API_KEY}&from=${from}&to=${to}&amount=1&format=json`);
+        const data = response.data;
+        const toCoinCode = Object.keys(data.rates)[0];
+        const conversionObj = {
+            from: data.base_currency_code,
+            fromCode: data.base_currency_name,
+            rate: data.rates[toCoinCode].rate,
+            to: data.rates[toCoinCode].currency_name,
+            toCode: toCoinCode
+        };
+        const userToUpdate = await retrieveUser(id);
+        res.json(conversionObj);
+    }
+    catch (e) {
+        res.status(400).json({
+            error: true,
+            message: e
+        });
+    }
+});
+/// Get all the available coins in the API 
+// type Coin = {
+//   code: string,
+//   currency: string
+// }
+// app.post('/insertCoins', async (req,res) => {
+//   const data = await axios.get(`https://api.getgeoapi.com/v2/currency/list?api_key=${process.env.API_KEY}&format=json`)
+//   const coins:Coin = data.data.currencies
+//   const coinsArr:Array<Coin> = []
+//   for(let coin of Object.entries(coins)){
+//     coinsArr.push({
+//       code: coin[0],
+//       currency: coin[1]
+//     })
+//   }
+//   const sorted = coinsArr.sort((a,b) => {
+//     return a.code.charCodeAt(0) - b.code.charCodeAt(0)
+//   })
+//   await usePrisma(async () => {
+//     await prisma.currency.deleteMany({})
+//     await prisma.currency.createMany({
+//       data: sorted
+//     })
+//   })
+//   res.json(sorted)
+//   let resp;
+//   await usePrisma(async () => {
+//     const coins = await prisma.currency.findMany({})
+//     resp = coins
+//   })
+//   res.json(resp)
+// })
+app.listen(process.env.PORT || 3000);
---
+++ index.ts
@@ -0,0 +1,218 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+import { AxiosInstance } from "axios";
+import { Application } from "express";
+import * as bcrypt from 'bcrypt'
+
+const express = require('express')
+const cors = require('cors')
+const app:Application = express()
+const axios:AxiosInstance = require('axios').default
+const prisma = new PrismaClient()
+
+
+app.use(express.json())
+
+app.use(cors())
+
+type Login = {
+  email: string,
+  password: string
+}
+  
+type User = Login & {
+  id: string
+  name: string
+}
+
+type FrontEndUserData = {
+  id: string,
+  name: string,
+  email: string
+}
+
+type ErrorMessage = {
+  error: boolean,
+  message: string
+}
+
+type Conversion = {
+  from: string,
+  fromCode: string,
+  rate: number,
+  to: string,
+  toCode: string
+}
+
+async function usePrisma(callback:Function){
+  await prisma.$connect()
+
+  await callback()
+
+  await prisma.$disconnect()
+}
+
+async function hashPassword(password: string):Promise<string>{
+  const hashedPassword = await bcrypt.hash(password, 1)
+  return hashedPassword
+}
+
+async function comparePassword(password: string, hashToCheck: string):Promise<boolean>{
+  const isPasswordValid = await bcrypt.compare(password,hashToCheck)
+  return isPasswordValid
+}
+
+async function registerUser({name, email, password}: User):Promise<FrontEndUserData|ErrorMessage>{
+  const hashedPassword = await hashPassword(password)
+
+  const dataToSend:Prisma.UserCreateInput = {
+    name,
+    email,
+    password: hashedPassword
+  }
+
+  let createdUser:FrontEndUserData|null = null
+
+  await usePrisma(async () => {
+    const dbUser = await prisma.user.create({
+      data: dataToSend
+    })
+    createdUser = {
+      id: dbUser.id,
+      name: dbUser.name,
+      email: dbUser.email
+    }
+  })
+
+  if(createdUser != null){
+    return createdUser
+  } else {
+    return {
+      error: true,
+      message: 'Error on creating user!'
+    }
+  }
+}
+
+async function checkUser({email, password}:Login):Promise<ErrorMessage|FrontEndUserData>{
+  let user:User[] = [];
+  await usePrisma(async () => {
+    user = await prisma.user.findMany({
+      where: {
+        email
+      }
+    })
+  })
+
+  const isLoginValid = await comparePassword(password, user[0].password)
+
+  if(isLoginValid){
+    return {
+      id: user[0].id,
+      name: user[0].name,
+      email: user[0].email
+    }
+  } else {
+    return {
+      error: true,
+      message: 'Wrong credentials!'  
+    }
+  }
+}
+
+async function retrieveUser(id:string):Promise<User>{
+  let user:User[] = []; 
+  console.log(user)
+  await usePrisma(async () => {
+    const dbSearch = await prisma.user.findMany({
+      where: {
+        id: id
+      }
+    })
+    user = dbSearch
+  })
+  return user[0]
+}
+
+app.post('/registerUser', async (req,res) => {
+  const data:User = req.body
+  try {
+    const userCreation = await registerUser(data)
+    res.json(userCreation)
+  } catch(e){
+    res.status(400).json(e)
+  }
+})
+
+app.post('/login', async(req,res) => {
+  const data:Login = req.body
+  
+  const result = await checkUser(data)
+
+  res.json(result)
+})
+
+app.post('/convert', async (req, res) => {
+  const {from, to, id} = req.body
+  if(typeof from == null || typeof to == null){
+    return res.status(400).json({
+      error: true,
+      message: "Please select the coins to convert!"
+    })
+  }
+  try{
+    const response = await axios.get(`https://api.getgeoapi.com/v2/currency/convert?api_key=${process.env.API_KEY}&from=${from}&to=${to}&amount=1&format=json`)
+    const data = response.data
+    const toCoinCode = Object.keys(data.rates)[0]
+    const conversionObj:Conversion = {
+      from: data.base_currency_code,
+      fromCode: data.base_currency_name,
+      rate: data.rates[toCoinCode].rate,
+      to: data.rates[toCoinCode].currency_name,
+      toCode: toCoinCode
+    }
+    const userToUpdate = await retrieveUser(id)
+    res.json(conversionObj)
+  } catch(e){
+    res.status(400).json({
+      error: true,
+      message: e
+    })
+  }
+}) 
+
+/// Get all the available coins in the API 
+
+// type Coin = {
+//   code: string,
+//   currency: string
+// }
+
+// app.post('/insertCoins', async (req,res) => {
+//   const data = await axios.get(`https://api.getgeoapi.com/v2/currency/list?api_key=${process.env.API_KEY}&format=json`)
+//   const coins:Coin = data.data.currencies
+//   const coinsArr:Array<Coin> = []
+//   for(let coin of Object.entries(coins)){
+//     coinsArr.push({
+//       code: coin[0],
+//       currency: coin[1]
+//     })
+//   }
+//   const sorted = coinsArr.sort((a,b) => {
+//     return a.code.charCodeAt(0) - b.code.charCodeAt(0)
+//   })
+//   await usePrisma(async () => {
+//     await prisma.currency.deleteMany({})
+//     await prisma.currency.createMany({
+//       data: sorted
+//     })
+//   })
+//   res.json(sorted)
+//   let resp;
+//   await usePrisma(async () => {
+//     const coins = await prisma.currency.findMany({})
+//     resp = coins
+//   })
+//   res.json(resp)
+// })
+
+app.listen(process.env.PORT || 3000)
